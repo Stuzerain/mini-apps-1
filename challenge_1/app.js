e812a6092e334063a2cc04f28deb8adedcbebc22
Stuzerain <shanford3@gmail.com>
2020-09-18 14:29:57 -0700

    Add ability to track row and col of space, consistency with model and view

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -6,25 +6,26 @@ var TTT = function () {
   // property to keep track of whose turn it is -- x when true, o when false
   this.xTurn = true;
 
-  // simple 3x3 matrix representing a board
+  // simple 3x3 matrix representing a board for setup and to keep track
+  // of state of the board
   this.board = [
-    [{ val: '&nbsp' }, { val: '&nbsp' }, { val: '&nbsp' }],
-    [{ val: '&nbsp' }, { val: '&nbsp' }, { val: '&nbsp' }],
-    [{ val: '&nbsp' }, { val: '&nbsp' }, { val: '&nbsp' }]
+    ['&nbsp', '&nbsp', '&nbsp'],
+    ['&nbsp', '&nbsp', '&nbsp'],
+    ['&nbsp', '&nbsp', '&nbsp']
   ];
 
   // function that draws board to html page
+  // fills table with values from
   this.drawBoard = () => {
     // debugger
     var loc = document.getElementById('boardGuts');
-    this.board.forEach((row) => {
+    this.board.forEach((row, index) => {
       var newRow = loc.insertRow(-1);
       for (var i = 0; i < row.length; i++) {
         var newCell = newRow.insertCell(i)
-        newCell.innerHTML = row[i].val;
+        newCell.innerHTML = row[i];
         newCell.onclick = () => { this.toggle(event.target) };
-        // newCell.setAttribute('onclick', () => { this.toggle() })
-
+        newCell.setAttribute('gridspot', [index, i])
       }
       loc.appendChild(newRow);
     })
@@ -36,18 +37,27 @@ var TTT = function () {
   // function passed to board cells as onclick that toggles x or o and switches current turn
   this.toggle = (item) => {
     // debugger;
-    if (this.xTurn === true) {
-      item.innerHTML = 'X';
-    } else {
-      item.innerHTML = 'O';
+    var row = item.attributes.gridspot.value[0];
+    var col = item.attributes.gridspot.value[2];
+    // first checks if space is empty -- only does anything if space is empty to
+    // prevent overwriting another player's space
+    if (item.innerHTML === '&nbsp;') {
+      if (this.xTurn === true) {
+        item.innerHTML = 'X';
+        this.board[row][col] = 'X';
+      } else {
+        item.innerHTML = 'O';
+        this.board[row][col] = 'O';
+      }
+      console.log(row, col)
+      this.xTurn = !this.xTurn;
     }
-    console.log(this.xTurn)
-    this.xTurn = !this.xTurn;
   }
 
+  // function passed to clear button at end of board that empties every space on board
   this.clearBoard = () => {
     // debugger;
-    console.log('attempting a clear')
+    console.log('attempting a clear');
     var loc = document.getElementById('board');
     for (var i = 0; i < loc.rows.length; i++) {
       for (var j = 0; j < loc.rows[i].cells.length; j++) {
