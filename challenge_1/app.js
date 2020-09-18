0d929c9ffaa00c71b6d2c14616153d7d552cca1d
Stuzerain <shanford3@gmail.com>
2020-09-18 14:14:46 -0700

    Implement method to clear board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,11 +2,15 @@ console.log('JS script loaded')
 
 // class constructor for a tic tac toe board
 var TTT = function () {
+
+  // property to keep track of whose turn it is -- x when true, o when false
+  this.xTurn = true;
+
   // simple 3x3 matrix representing a board
   this.board = [
-    [{ val: '1' }, { val: '2' }, { val: '3' }],
-    [{ val: '4' }, { val: '5' }, { val: '6' }],
-    [{ val: '7' }, { val: '8' }, { val: '9' }]
+    [{ val: '&nbsp' }, { val: '&nbsp' }, { val: '&nbsp' }],
+    [{ val: '&nbsp' }, { val: '&nbsp' }, { val: '&nbsp' }],
+    [{ val: '&nbsp' }, { val: '&nbsp' }, { val: '&nbsp' }]
   ];
 
   // function that draws board to html page
@@ -24,35 +28,58 @@ var TTT = function () {
       }
       loc.appendChild(newRow);
     })
+
+    var butt = document.getElementById('clear');
+    butt.onclick = () => { this.clearBoard() };
   }
 
+  // function passed to board cells as onclick that toggles x or o and switches current turn
   this.toggle = (item) => {
-    console.log(item)
+    // debugger;
+    if (this.xTurn === true) {
+      item.innerHTML = 'X';
+    } else {
+      item.innerHTML = 'O';
+    }
+    console.log(this.xTurn)
+    this.xTurn = !this.xTurn;
   }
 
-  // property to keep track of whose turn it is -- x when true, o when false
-  this.xTurn = true;
-}
-
-
-var drawBoard = (data) => {
-  // debugger
-  var loc = document.getElementById('Tic');
-  data.forEach((row) => {
-    var newRow = loc.insertRow(-1);
-    for (var i = 0; i < row.length; i++) {
-      var newCell = newRow.insertCell(i)
-      newCell.innerHTML = row[i].val;
+  this.clearBoard = () => {
+    // debugger;
+    console.log('attempting a clear')
+    var loc = document.getElementById('board');
+    for (var i = 0; i < loc.rows.length; i++) {
+      for (var j = 0; j < loc.rows[i].cells.length; j++) {
+        loc.rows[i].cells[j].innerHTML = '&nbsp';
+      }
     }
-    loc.appendChild(newRow);
-  })
+  }
+
+
 }
+
+// initialize game
+var TicTac = new TTT();
+TicTac.drawBoard();
+
+// var drawBoard = (data) => {
+//   // debugger
+//   var loc = document.getElementById('Tic');
+//   data.forEach((row) => {
+//     var newRow = loc.insertRow(-1);
+//     for (var i = 0; i < row.length; i++) {
+//       var newCell = newRow.insertCell(i)
+//       newCell.innerHTML = row[i].val;
+//     }
+//     loc.appendChild(newRow);
+//   })
+// }
 
 
 // functions that are run when the page loads
 // var loadHerUp = function () {
-var TicTac = new TTT();
-TicTac.drawBoard();
+
 // }
 
 // window.onload = loadHerUp;
